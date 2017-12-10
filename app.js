55830b0ba7deb71667030e2e2bc99fdf875966c5
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2017-12-09 18:15:35 -0800

    added firebase

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
 var config = {
-    apiKey: "AIzaSyC9JPVt0aiHlf_cyk5wqkmlJfw77H5L6sI",
-    authDomain: "jm-coding.firebaseapp.com",
-    databaseURL: "https://jm-coding.firebaseio.com",
-    projectId: "jm-coding",
-    storageBucket: "jm-coding.appspot.com",
-    messagingSenderId: "356825613576"
+    apiKey: "AIzaSyAF86Bjo94e45Kyuyeyve-VesbVzMHe_3E",
+    authDomain: "trainhw-af312.firebaseapp.com",
+    databaseURL: "https://trainhw-af312.firebaseio.com",
+    projectId: "trainhw-af312",
+    storageBucket: "",
+    messagingSenderId: "740282333523"
   };
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 var database = firebase.database();
 
-    
-        $("#search-btn").on("click", function (event) {
-        event.preventDefault();
-      
-     
-      var employeeData =($("#name").val());
-    
-      var roleData = ($("#role").val());
-     
-      var startYearData = ($("#start-yr").val());
-     
-    
-    
-      
-      database.ref().push({
-      employee: employeeData,
-      role: roleData,
-      startYear: startYearData,
-      dateAdded: firebase.database.ServerValue.TIMESTAMP
-      });
-      
-      });         
-      
-      database.ref().on("child_added", function (childSnapshot) {
-        
-        console.log(childSnapshot.val().employee);
-        console.log(childSnapshot.val().role);
-        console.log(childSnapshot.val().startYear);
-        console.log(childSnapshot.val().dateAdded);
-        
-        var row = $("<tr>"); 
-        row.append("<td id='employee_'> " + childSnapshot.val().employee + 
-                   " </td><td id = 'role_'> " + childSnapshot.val().role + 
-                   " </td><td id = 'startYear_'> " + childSnapshot.val().startYear + 
-                   " </td><td id = 'dateAdded_'> " + childSnapshot.val().dateAdded + " </td>");
-          $("#tableBody").append(row);
-        });
+
+$("#search-btn").on("click", function (event) {
+    event.preventDefault();
+
+
+    var employeeData = ($("#name").val());
+
+    var roleData = ($("#role").val());
+
+    var startYearData = ($("#start-yr").val());
+
+
+
+
+    database.ref().push({
+        employee: employeeData,
+        role: roleData,
+        startYear: startYearData,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+
+});
+
+database.ref().on("child_added", function (childSnapshot) {
+
+    console.log(childSnapshot.val().employee);
+    console.log(childSnapshot.val().role);
+    console.log(childSnapshot.val().startYear);
+    console.log(childSnapshot.val().dateAdded);
+
+    var row = $("<tr>");
+    row.append("<td id='employee_'> " + childSnapshot.val().employee +
+        " </td><td id = 'role_'> " + childSnapshot.val().role +
+        " </td><td id = 'startYear_'> " + childSnapshot.val().startYear +
+        " </td><td id = 'dateAdded_'> " + childSnapshot.val().dateAdded + " </td>");
+    $("#tableBody").append(row);
+});
