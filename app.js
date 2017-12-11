69f4513d6aa14a6845a7766e7874d29d0325baaa
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2017-12-11 14:59:29 -0800

    added object to app.js

--- app.js
+++ app.js
@@ -14,30 +14,60 @@ $("#search-btn").on("click", function (event) {
     event.preventDefault();
 
 
-    var employeeData = ($("#name").val());
+    var trainName = ($("#name").val());
 
-    var roleData = ($("#role").val());
+    var trainTime = ($("#time").val());
 
-    var startYearData = ($("#start-yr").val());
+    var tFrequency = ($("#freq").val());
 
 
 
 
     database.ref().push({
-        employee: employeeData,
-        role: roleData,
-        startYear: startYearData,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
+        name: trainName,
+        time: roleData,
+        freq: tFrequency,
+        timeAdded: firebase.database.ServerValue.TIMESTAMP
     });
 
 });
 
 database.ref().on("child_added", function (childSnapshot) {
 
-    console.log(childSnapshot.val().employee);
-    console.log(childSnapshot.val().role);
-    console.log(childSnapshot.val().startYear);
-    console.log(childSnapshot.val().dateAdded);
+    console.log(childSnapshot.val().name);
+    console.log(childSnapshot.val().time);
+    console.log(childSnapshot.val().freq);
+    console.log(childSnapshot.val().timeAdded);
+
+        // Assumptions
+        var tFrequency = 15;
+        
+            // Time is 3:30 AM
+            var firstTime = "03:30";
+        
+            // First Time (pushed back 1 year to make sure it comes before current time)
+            var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+            console.log(firstTimeConverted);
+        
+            // Current Time
+            var currentTime = moment();
+            console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+        
+            // Difference between the times
+            var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+            console.log("DIFFERENCE IN TIME: " + diffTime);
+        
+            // Time apart (remainder)
+            var tRemainder = diffTime % tFrequency;
+            console.log(tRemainder);
+        
+            // Minute Until Train
+            var tMinutesTillTrain = tFrequency - tRemainder;
+            console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+        
+            // Next Train
+            var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+            console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
     var row = $("<tr>");
     row.append("<td id='employee_'> " + childSnapshot.val().employee +
